48309177bdaf6ed46731f9b0e5fad164b81d2c4f
BadDreamsss <leonidpanov54@gmail.com>
2024-04-10 20:45:40 +0300

    Initial commit

---
+++ c-server/src/test/java/ru/computerclub/backend/cserver/CServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.computerclub.backend.cserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
